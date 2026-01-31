a58e01083622f460426f88d4609989ea12b0de3d
djumanov <djumanovdev@gmail.com>
2026-01-31 19:48:53 +0500

    task ;

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import requests
 
 
-url = 'https://randomuser.me/api/'
+def get_randomuser_full_data():
+    url = 'https://randomuser.me/api/'
 
-response = requests.get(url)
-data = response.json()
+    # response = requests.get(url)
+    # data = response.json()
 
-print(data['results'][0]['name'])
+    # print(data['results'][0]['name'])
+
+
+def get_user_data(user: dict):
+    pass
+
+
+def main() -> None:
+    pass
+
+main()
